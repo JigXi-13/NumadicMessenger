4cf850eb969adc825c6a26b211019bfbdc6c0af7
JigXi-13 <155010495+JigXi-13@users.noreply.github.com>
2024-06-30 16:17:31 +0530

    created API for adding user, removing user, renaming group

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -5,6 +5,9 @@ const {
   accessChat,
   fetchChats,
   createGroupChat,
+  renameGroup,
+  removeFromGroup,
+  addToGroup,
 } = require("../controllers/chatControllers");
 
 const router = express.Router();
@@ -12,5 +15,8 @@ const router = express.Router();
 router.route("/").post(protect, accessChat);
 router.route("/").get(protect, fetchChats);
 router.route("/group").post(protect, createGroupChat);
+router.route("/rename").put(protect, renameGroup);
+router.route("/groupremove").put(protect, removeFromGroup);
+router.route("/groupadd").put(protect, addToGroup);
 
 module.exports = router;
